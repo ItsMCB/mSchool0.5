c281fad76ea6270f15e7ae4a6ecc693a29c92c96
Martin Carl B <33915290+ItsMCB@users.noreply.github.com>
2018-03-03 13:12:02 -0600

    Create MCBAppOpenerV1.js

---
+++ MCBAppOpenerV1.js
@@ -0,0 +1,251 @@
+// MCB App Opener 1.0
+
+  var mSchool05;
+
+ function openmSchool05() {
+     mSchool05Window = window.open('https://itsmcb.github.io/mSchool0.5', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closemSchool05() {
+      mSchool05Window.close();
+  }
+
+// mSchool 0.5 Apps
+
+  var StylixWebapp;
+
+ function openStylixWebapp() {
+     StylixWebappWindow = window.open('https://itsmcb.github.io/mSchool0.5/stylixweb', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeStylixWebapp() {
+      StylixWebappWindow.close();
+  }
+
+
+  var Homeworkapp;
+
+ function openHomeworkapp() {
+     HomeworkappWindow = window.open('https://itsmcb.github.io/mSchool0.5/2Do', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeHomeworkapp() {
+      HomeworkappWindow.close();
+  }
+
+
+  var Mailapp;
+
+ function openMailapp() {
+     MailappWindow = window.open('https://mail.google.com', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeMailapp() {
+      MailappWindow.close();
+  }
+
+
+  var GoogleClassroomapp;
+
+ function openGoogleClassroomapp() {
+     GoogleClassroomappWindow = window.open('https://classroom.google.com', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeGoogleClassroomapp() {
+      GoogleClassroomappWindow.close();
+  }
+
+
+  var ConnectEdMATHapp;
+
+ function openConnectEdMATHapp() {
+     ConnectEdMATHappWindow = window.open('https://connected.mcgraw-hill.com/connected/login.do', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeConnectEdMATHapp() {
+      ConnectEdMATHappWindow.close();
+  }
+
+
+  var PowerSchoolapp;
+
+ function openPowerSchoolapp() {
+     PowerSchoolappWindow = window.open('https://ps.d64.org/public/', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closePowerSchoolapp() {
+      PowerSchoolappWindow.close();
+  }
+
+
+  var GoogleDriveapp;
+
+ function openGoogleDriveapp() {
+     GoogleDriveappWindow = window.open('https://drive.google.com', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeGoogleDriveapp() {
+      GoogleDriveappWindow.close();
+  }
+
+
+  var NotThisapp;
+
+ function openNotThisapp() {
+     NotThisappWindow = window.open('https://itsmcb.github.io/mSchool0.5/notethis', '_blank', 'toolbar=0,location=0,menubar=0');
+ }
+ function closeNotThisapp() {
+      NotThisappWindow.close();
+  }
+
+// Non MCB Default Apps
+
+var QuizletappWindow;
+
+function openQuizletapp() {
+    QuizletappWindow = window.open('https://quizlet.com/latest/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeQuizletapp() {
+    QuizletappWindow.close();
+}
+
+
+var GoogleDocsappWindow;
+
+function openGoogleDocsapp() {
+    GoogleDocsappWindow = window.open('https://docs.google.com/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeGoogleDocsapp() {
+    GoogleDocsappWindow.close();
+}
+
+
+
+var GoogleSheetsappWindow;
+
+function openGoogleSheetsapp() {
+    GoogleSheetsappWindow = window.open('https://docs.google.com/spreadsheets/u/0/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeGoogleSheets() {
+    GoogleSheetsappWindow.close();
+}
+
+
+
+var GoogleSlidesappWindow;
+
+function openGoogleSlidesapp() {
+    GoogleSlidesappWindow = window.open('https://docs.google.com/presentation/u/0/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeGoogleSlidesapp() {
+    GoogleSlidesappWindow.close();
+}
+
+
+var GoogleFormsappWindow;
+
+function openGoogleFormsapp() {
+    GoogleFormsappWindow = window.open('https://docs.google.com/forms/u/0/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeGoogleFormsapp() {
+    GoogleFormsappWindow.close();
+}
+
+
+var RanderappWindow;
+
+function openRanderapp() {
+    RanderappWindow = window.open('https://itsmcb.github.io/mSchool0.5/Rander', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeRanderapp() {
+    RanderappWindow.close();
+}
+
+
+var GoogleEarthappWindow;
+
+function openGoogleEarthapp() {
+	GoogleEarthappWindow = window.open('https://earth.google.com/web/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeGoogleEarthapp() {
+	GoogleEarthappWindow.close();
+}
+
+
+var Cnn10appWindow;
+
+function openCnn10app() {
+	Cnn10appWindow = window.open('http://www.cnn.com/cnn10', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeCnn10app() {
+	Cnn10appWindow.close();
+}
+
+
+var PixlrappWindow;
+
+function openPixlrapp() {
+	PixlrappWindow = window.open('https://pixlr.com/editor/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closePixlrapp() {
+	PixlrappWindow.close();
+}
+
+
+
+var KhanAcademyappWindow;
+
+function openKhanAcademyapp() {
+	KhanAcademyappWindow = window.open('https://www.khanacademy.org/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeKhanAcademyapp() {
+	KhanAcademyappWindow.close();
+}
+
+
+var MozillaRTEappWindow;
+
+function openMozillaRTEapp() {
+	MozillaRTEappWindow = window.open('https://itsmcb.github.io/mSchool0.5/Mozilla%20Rich%20Text%20Editor%20for%20mSchool', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeMozillaRTEapp() {
+	MozillaRTEappWindow.close();
+}
+
+
+var CalcappWindow;
+
+function openCalcapp() {
+	CalcappWindow = window.open('https://itsmcb.github.io/mSchool0.5/calculator', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeCalcapp() {
+	CalcappWindow.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Custom Windows added by District
+
+var LMSStudentPortalappWindow;
+
+function openLMSStudentPortalapp() {
+	LMSStudentPortalappWindow = window.open('https://sites.google.com/a/d64.org/lincoln-student-portal/', '_blank', 'toolbar=0,location=0,menubar=0');
+}
+
+function closeLMSStudentPortalapp() {
+	LMSStudentPortalappWindow.close();
+}
